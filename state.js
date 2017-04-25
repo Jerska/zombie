1d925b78f35643ccb0f55aa7c8850fb6d53b48f8
mathon_j <mathon_j@epitech.eu>
2017-04-25 22:08:47 +0200

    function GenerateMonster

--- state.js
+++ state.js
@@ -1,12 +1,28 @@
 const MAP_SIZE = 48;
 
+let queue_monster = [];
+
 function createMap () {
   return new Array(MAP_SIZE).fill(null).map(() => (
     new Array(MAP_SIZE).fill(null).map(() => ({ color: '#555' }))
   ));
 }
 
+function generateMonster() {
+    min = Math.ceil(0);
+    max = Math.floor(MAP_SIZE);
+    let monster = {
+        x: Math.floor(Math.random() * (max - min -16 +1)) + min,
+        y: Math.floor(Math.random() * (max - min -24 +1)) + min
+    };
+    console.log(`Monster position ${monster.x} and ${monster.y}`);
+    queue_monster.push(monster);
+}
+
 console.log(createMap());
+generateMonster();
+generateMonster();
+generateMonster();
 
 module.exports = {
   game: {

fe3fe84b09ea57ce89b331aa734114398d21513d
Matthieu Dumont <jerska@live.fr>
2017-04-25 19:52:27 +0200

    chore(frontend): basic index with livereload

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ app.use(express.static('public'));
 
 const server = http.createServer(app);
 
+if (app.get('env') === 'development') {
+  const reload = require('reload');
+  reload(server, app, true);
+
+  const livereload = require('livereload');
+  const lrserver = livereload.createServer();
+  lrserver.watch(__dirname + '/public');
+}
+
+app.get('/constants.js', (req, res) => {
+  res.contentType('application/javascript');
+  res.send('' +
+    'window.CONSTANTS = {' +
+    '  env: "' + app.get('env') + '"' +
+    '}'
+  );
+});
+
 server.listen(app.get('port'), () => {
   console.info(`Running on http://localhost:${app.get('port')}/`);
 });
